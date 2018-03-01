deba8c0c23df79a59b0e0a866d75d4a930f35f92
robertvsiii <robertvsiii@gmail.com>
2018-03-01 10:27:03 -0500

    c code for finding cycles
    added c code for finding cycles; should be faster than Matlab

---
+++ cycle_finder/find_acc3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <fstream>
+#include <vector>
+#include <algorithm> // for std::copy
+
+int main()
+{
+    std::ifstream infile("A.csv");
+    std::vector< std::vector<bool> > A;
+    std::vector< std::vector<int> > cycles;
+    std::vector<int> reactions (3,0);
+
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        std::istringstream iss(line);
+        std::string el;
+        std::vector<bool> numbers;
+        while (std::getline(iss,el,',')){
+            numbers.push_back( el == "1");
+        }
+        A.push_back(numbers);
+    }
+    
+    for(int i = 0; i < A.size(); i++){
+        for(int j = 0; j < i; j++){
+            for(int k = 0; k < j; k++){
+                if(A[i][j]*A[j][k]*A[k][i] == 1){
+                    reactions[0] = i+1;
+                    reactions[1] = j+1;
+                    reactions[2] = k+1;
+                    cycles.push_back(reactions);
+                }
+            }
+        }
+    }
+    
+    std::ofstream outfile;
+    outfile.open ("acc3.csv");
+    for(int j = 0; j < cycles.size(); j++){
+        for(int m = 0; m < cycles[j].size(); m++){
+            outfile << cycles[j][m] << ",";
+        }
+        outfile << "\n";
+    }
+    outfile.close();
+    
+}
